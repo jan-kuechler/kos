10db64ca41d0047d75bbe1125ed3cb42fe7fa974
kuchen <kuchen_@gmx.de>
2009-07-13 20:08:45 +0200

    - com module now defines device files (write only at the moment)

--- kernel/com.c
+++ kernel/com.c
@@ -3,6 +3,10 @@
 #include <ports.h>
 #include "bios.h"
 #include "com.h"
+#include "fs/types.h"
+#include "fs/devfs.h"
+
+#define
 
 #define TRANSMIT     0
 #define INTR_ENABLE  1
@@ -20,6 +24,28 @@ static int ports[4] = {
 	0x2F8,
 	0x3E8,
 	0x2E8,
+};
+
+static struct inode inodes[COM_MAX_PORTS];
+
+static char *names[COM_MAX_PORTS] = {
+	"com0", "com1", "com2", "com3",
+};
+
+static int com_open(struct inode *ino, struct file *file, dword flags);
+
+static int com_close(struct file *file);
+static int com_read(struct file *file, void *bufer, dword count, dword offset);
+static int com_write(struct file *file, void *buffer, dword count, dword offset);
+
+static struct inode_ops com_ino_ops = {
+	.open = com_open,
+};
+
+static struct file_ops com_file_ops = {
+	.close = com_close,
+	.read  = com_read,
+	.write = com_write,
 };
 
 #define USABLE(p) (ports[p] != 0)
@@ -51,16 +77,22 @@ static void init_port(int p, int baud, int parity, int bits)
 
 void init_com(void)
 {
-	/* TODO: Get real base ports from BIOS */
-
 	struct bios_data_area *info = (struct bios_data_area*)BIOS_DATA_ADDR;
 
 	int i=0;
 	for (; i < 4; ++i) {
 		ports[i] = info->com_io[i];
 
-		if (ports[i] != 0) {
+		if (USABLE(i)) {
 			init_port(i, COM_BAUD, COM_PARITY, COM_BITS);
+
+			memset(inodes[i], 0, sizeof(struct inode));
+			inodes[i].name  = names[i];
+			inodes[i].flags = FS_CHARDEV;
+			inodes[i].impl  = i;
+			inodes[i].ops   = &com_ino_ops;
+
+			devfs_register(&inodes[i]);
 		}
 	}
 }
@@ -92,3 +124,33 @@ void com_puts(int port, const char *str)
 	}
 }
 
+static int com_open(struct inode *ino, struct file *file, dword flags)
+{
+	file->pos = 0;
+	file->fops = &com_file_ops;
+
+	return 0;
+}
+
+static int com_close(struct file *file)
+{
+	return 0;
+}
+
+static int com_read(struct file *file, void *buffer, dword count, dword offset)
+{
+	return 0;
+}
+
+static int com_write(struct file *file, void *buffer, dword count, dword offset)
+{
+	int id = file->impl;
+
+	char *buf = buffer;
+	int i=0;
+	for (; i < count; ++i) {
+		com_putc(id, buf[i]);
+	}
+
+	return count;
+}
--- kernel/include/com.h
+++ kernel/include/com.h
@@ -1,6 +1,8 @@
 #ifndef COM_H
 #define COM_H
 
+#define COM_MAX_PORTS 4
+
 #define COM_BAUD   38400
 #define COM_PARITY 0
 #define COM_BITS   8
