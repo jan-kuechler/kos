e4748c149574156ec72ee009e26a2a5c73f035d3
kuchen <kuchen_@gmx.de>
2009-07-15 15:36:05 +0200

    - added memsetw to libminc and moved extensions to minc.h

--- include/format.h
+++ include/format.h
@@ -1,13 +1,1 @@
-#ifndef FORMAT_H
-#define FORMAT_H
-
-#include <stdarg.h>
-
-/* see lib/libminc/format.c */
-
-int numfmt(char *buffer, int num, int base, int pad, char pc);
-
-int strfmt(char *buffer, const char *fmt, ...);
-int strafmt(char *buffer, const char *fmt, va_list args);
-
-#endif /*FORMAT_H*/
+#include <minc.h>
